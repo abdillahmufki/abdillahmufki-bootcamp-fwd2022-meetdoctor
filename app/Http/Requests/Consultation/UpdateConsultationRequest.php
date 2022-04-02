39ec6b78bf41fb3d01785c6c39278e63ff5f46af
abdillahmufki <abdillahmufki123@gmail.com>
2022-04-02 19:30:46 +0700

    Make controller and slicing

--- app/Http/Requests/Consultation/UpdateConsultationRequest.php
+++ app/Http/Requests/Consultation/UpdateConsultationRequest.php
@@ -7,6 +7,9 @@ use App\Models\MasterData\Consultation;
 use Illuminate\Foundation\Http\FormRequest;
 use Symfony\Component\HttpFoundation\Response;
 
+// this rule only at update request
+use Illuminate\Validation\Rule;
+
 class UpdateConsultationRequest extends FormRequest
 {
     /**
@@ -28,7 +31,7 @@ class UpdateConsultationRequest extends FormRequest
     {
         return [
             'name' => [
-                'required', 'string', 'max:255',
+                'required', 'string', 'max:255', Rule::unique('consultation')->ignore($this->consultation),
             ],
         ];
     }
